9f4ec67ab7532688462342fd1cb0847b52edfb28
lightrao <lightrao@hotmail.com>
2024-07-01 22:21:37 +0800

    add comments for Abi

--- utils/AbiList.js
+++ utils/AbiList.js
@@ -1,12 +1,30 @@
 const erc20ABI = ["function decimals() external view returns (uint8)"];
 
-// uniswap v2 ABI
+// ABI for Uniswap V2 Factory Contract
 const factoryABI = [
+  // This function takes two token addresses (tokenA and tokenB) and returns the address of the pair contract for those tokens
+  // It is marked as 'external', meaning it can be called from outside the contract
+  // It is marked as 'view', meaning it does not modify the state of the blockchain
   "function getPair(address tokenA, address tokenB) external view returns (address pair)",
 ];
 
+// ABI for Uniswap V2 Router Contract
 const routerABI = [
+  // This function takes an input amount of tokens and a path (an array of token addresses)
+  // It returns the output amounts for each token in the path
+  // It is marked as 'public', meaning it can be called both from inside and outside the contract
+  // It is marked as 'view', meaning it does not modify the state of the blockchain
   "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+
+  // This function swaps an exact amount of input tokens for as many output tokens as possible
+  // It takes the following parameters:
+  // - amountIn: the amount of input tokens to send
+  // - amountOutMin: the minimum amount of output tokens that must be received for the transaction not to revert
+  // - path: an array of token addresses (the swap route)
+  // - to: the address to send the output tokens to
+  // - deadline: a timestamp by which the transaction must be completed, otherwise it will revert
+  // It returns an array of amounts, representing the amount of each token received at each step of the swap
+  // It is marked as 'external', meaning it can be called from outside the contract
   "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
 ];
 
